2f3dc72949d6a18613a5bc4606284cdedea3e989
Rafael <rflernesto@gmail.com>
2021-07-31 18:06:02 -0400

    test: Ensure Book Entitiy returns an instance of Book when parameters are correct

---
+++ src/entities/__test__/book.spec.ts
@@ -0,0 +1,14 @@
+import { Book } from '../book'
+
+describe('Book entity tests', () => {
+    it('Ensure Book Entitiy returns an instance of Book when parameters are correct', () => {
+        const book = Book.create({
+            name: 'Test title',
+            edition: 123,
+            publication_year: 2021,
+            authors: ['id_1', 'id_2'],
+        })
+
+        expect(book).toBeInstanceOf(Book)
+    })
+})

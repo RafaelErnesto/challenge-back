f79d7654fefceeb0e00bfaa24ae74a557bfb73c2
Rafael <rflernesto@gmail.com>
2021-08-01 15:46:49 -0400

    test: Ensure GetBookController returns statusCode 200 and list of books

---
+++ src/adapters/presentation/controllers/get-book-list/get-book-list-controller.spec.ts
@@ -0,0 +1,19 @@
+import { GetBookList } from '../../../../usecases/get-book-list/get-book-list'
+import { BookInMemoryRepository } from '../../../../infra/data/in-memory/book-in-memory-repository'
+import { GetBookListController } from './get-book-list-controller'
+import { BaseController } from '../http/base-controller'
+
+describe('GetBookController test', () => {
+    const getSut = (): BaseController => {
+        const bookRepo = new BookInMemoryRepository()
+        const getBookList = new GetBookList(bookRepo)
+        const getAuthorListController = new GetBookListController(getBookList)
+        return getAuthorListController
+    }
+    it('Ensure GetBookController returns statusCode 200 and list of books', async () => {
+        const sut = getSut()
+        const books = await sut.handle({})
+        expect(books.statusCode).toBe(200)
+        expect(books.body.length).toEqual(2)
+    })
+})

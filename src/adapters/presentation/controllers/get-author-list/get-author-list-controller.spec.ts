87ab5fa5bc3e4055ba0f528d888b246ac87f1aef
Rafael <rflernesto@gmail.com>
2021-08-01 13:19:34 -0400

    test: Should return statusCode 200 and authors list

---
+++ src/adapters/presentation/controllers/get-author-list/get-author-list-controller.spec.ts
@@ -0,0 +1,19 @@
+import { AuthorInMemoryRepository } from '../../../../infra/data/in-memory/author-in-memory-repository'
+import { GetAuthorList } from '../../../../usecases/get-author-list/get-author-list'
+import { BaseController } from '../http/base-controller'
+import { GetAuthorListController } from './get-author-list-controller'
+
+describe('GetAuthorListController test', () => {
+    const getSut = (): BaseController => {
+        const authorRepo = new AuthorInMemoryRepository()
+        const getAuthorList = new GetAuthorList(authorRepo)
+        const getAuthorListController = new GetAuthorListController(getAuthorList)
+        return getAuthorListController
+    }
+    it('Should return statusCode 200 and authors list', async () => {
+        const sut = getSut()
+        const response = await sut.handle({})
+        expect(response.statusCode).toBe(200)
+        expect(response.body.length).toBeGreaterThan(0)
+    })
+})

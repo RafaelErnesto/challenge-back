fe8f8296c2604d11e73f8f120085c0f5b124795c
Rafael <rflernesto@gmail.com>
2021-08-01 14:01:45 -0400

    test: Get author filtered author list

--- src/usecases/get-author-list/get-author-list.spec.ts
+++ src/usecases/get-author-list/get-author-list.spec.ts
@@ -1,10 +1,20 @@
 import { AuthorInMemoryRepository } from '../../infra/data/in-memory/author-in-memory-repository'
 import { GetAuthorList } from './get-author-list'
+import { GetAuthorListUseCase } from './get-author-list-use-case'
 describe('Get author list test', () => {
-    it('Get author list', async () => {
+    const getSut = (): GetAuthorListUseCase => {
         const inMemoryRepo = new AuthorInMemoryRepository()
-        const sut = new GetAuthorList(inMemoryRepo)
+       return  new GetAuthorList(inMemoryRepo)
+    }
+    it('Get author list', async () => {
+        const sut = getSut()
         const authors = await sut.getAuthorList()
-        expect(authors.length).toBeGreaterThan(0)
+        expect(authors.length).toBe(11)
+    })
+
+    it('Get author filtered author list', async () => {
+        const sut = getSut()
+        const authors = await sut.getAuthorList(1,'J')
+        expect(authors.length).toBe(3)
     })
 })

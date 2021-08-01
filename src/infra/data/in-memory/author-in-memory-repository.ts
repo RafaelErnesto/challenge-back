1fa135c36dd7a74ab55e3253bf931adc0bdbfddd
Rafael <rflernesto@gmail.com>
2021-07-31 23:06:44 -0400

    feat: creates method getAuthorById in AuthorInMemoryRepository

--- src/infra/data/in-memory/author-in-memory-repository.ts
+++ src/infra/data/in-memory/author-in-memory-repository.ts
@@ -7,6 +7,14 @@ export class AuthorInMemoryRepository implements AuthorRepositoryInterface {
     constructor() {
         this.authors = authorsData
     }
+    getAuthorById(id: any): Promise<AuthorResponseData> {
+        
+       const author =  this.authors.find((author: any) => {
+           return author.id === id
+       })
+       const response = author ?? null
+       return new Promise(resolve => resolve(response))
+    }
 
     getAllAuthors(): Promise<AuthorResponseData[]> {
         return new Promise((resolve,reject) => {

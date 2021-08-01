28ac2610d758351c45163f6718938f07915fdad7
Rafael <rflernesto@gmail.com>
2021-08-01 10:09:52 -0400

    refactor: use utils function validateAUthorsExists on registerBook

--- src/usecases/register-book/register-book.ts
+++ src/usecases/register-book/register-book.ts
@@ -3,7 +3,7 @@ import { AuthorRepositoryInterface } from "../ports/author-repository";
 import { BookRepositoryInterface } from "../ports/book-repository";
 import { BookResponseData } from "./book-response-data";
 import { RegisterBookUseCase } from "./register-book-use-case";
-
+import { validateAuthorsExists } from '../utils/validate-author-exists'
 export class RegisterBook implements RegisterBookUseCase {
     private bookRepository: BookRepositoryInterface
     private authorRepository: AuthorRepositoryInterface
@@ -16,28 +16,11 @@ export class RegisterBook implements RegisterBookUseCase {
     async registerBook(data: BookData): Promise<BookResponseData> {
         const bookEntity = Book.create(data)
         const book = await this.bookRepository.addBook(data)
-        const validationResult = await this.validateAuthors(data.authors)
+        const validationResult = await validateAuthorsExists(data.authors, this.authorRepository)
         if(!validationResult) {
             return new Promise((resolve, reject) => reject('Some of the authors were not found'))
         }
         return new Promise(resolve => resolve(book))
-    }
-
-    private async validateAuthors(authors: string[]): Promise<boolean> {
-        const result = await Promise.allSettled(authors.map((author) => {
-            return this.authorRepository.getAuthorById(author)
-        }))
-
-        const foundAuthors = result
-            .filter(data =>  data.status === 'fulfilled')
-            .filter(data =>  {
-                if(data.status === 'fulfilled') {
-                    return data.value !== null
-                }
-            })
-
-        const comparison =  foundAuthors.length === authors.length
-        return new Promise(resolve => resolve(comparison))
     }
 
 }

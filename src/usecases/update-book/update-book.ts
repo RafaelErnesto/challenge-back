7209b54e564cfe7a7229acbef151a98b36367ac3
Rafael <rflernesto@gmail.com>
2021-08-01 09:35:46 -0400

    feat: UpdateBook usecase rejects when some of the received authors do not exists

--- src/usecases/update-book/update-book.ts
+++ src/usecases/update-book/update-book.ts
@@ -1,16 +1,22 @@
 import { Book, BookData } from "../../entities/book";
+import { AuthorRepositoryInterface } from "../ports/author-repository";
 import { BookRepositoryInterface } from "../ports/book-repository";
 import { BookResponseData } from "../register-book/book-response-data";
 import { UpdateBookUseCase } from "./update-book-use-case";
-
+import { validateAuthorsExists }   from '../utils/validate-author-exists'
 export class UpdateBook implements UpdateBookUseCase {
     private bookRepository: BookRepositoryInterface
+    private authorRepository: AuthorRepositoryInterface
 
-    constructor(bookRepo: BookRepositoryInterface) {
+    constructor(bookRepo: BookRepositoryInterface, authorRepository: AuthorRepositoryInterface) {
         this.bookRepository = bookRepo
+        this.authorRepository = authorRepository
     }
 
     async updateBook(data: BookData, id: string): Promise<BookResponseData> {
+        if(!await validateAuthorsExists(data.authors, this.authorRepository)) {
+            return new Promise((resolve,reject) => reject('Some authors were not found'))
+        }
         const book = Book.create(data)
         const updatedBook = await this.bookRepository.updateBook(book, id)
         return new Promise(resolve => resolve(updatedBook))

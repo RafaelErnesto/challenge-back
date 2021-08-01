88b9e968e767c5c49b2131fa65efb2a057521e87
Rafael <rflernesto@gmail.com>
2021-08-01 09:34:56 -0400

    test: Ensure UpdateBook usercase throws when author do not exist

--- src/usecases/update-book/update-book.spec.ts
+++ src/usecases/update-book/update-book.spec.ts
@@ -1,11 +1,13 @@
 import { UpdateBookUseCase } from './update-book-use-case'
 import { BookInMemoryRepository } from '../../infra//data/in-memory/book-in-memory-repository'
+import { AuthorInMemoryRepository } from '../../infra//data//in-memory/author-in-memory-repository'
 import { UpdateBook } from './update-book'
 
 describe('Update Book Test', () => {
     const getSut = (): UpdateBookUseCase => {
         const bookRepo = new BookInMemoryRepository()
-        const sut = new UpdateBook(bookRepo)
+        const authorRepo = new AuthorInMemoryRepository()
+        const sut = new UpdateBook(bookRepo, authorRepo)
         return sut
     }
     it('Ensure UpdateBook usercase returns updated book on success', async () => {
@@ -19,5 +21,16 @@ describe('Update Book Test', () => {
 
         expect(result.publication_year).toEqual(2011)
         expect(result.name).toEqual('Update')
+    })
+
+    it('Ensure UpdateBook usercase throws when author do not exist', () => {
+        const sut = getSut()
+        expect(async () => {await sut.updateBook({
+            name: "Update",
+            publication_year: 2011,
+            edition:1,
+            authors:["test"]
+        },"asm3")}
+        ).rejects.toMatch('Some authors were not found')
     })
 })

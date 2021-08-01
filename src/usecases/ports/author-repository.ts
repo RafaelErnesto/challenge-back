757e0320629e45ad558350595df5e7e60840e3e0
Rafael <rflernesto@gmail.com>
2021-07-31 20:30:00 -0400

    feat: add author repository interface

---
+++ src/usecases/ports/author-repository.ts
@@ -0,0 +1,12 @@
+import { AuthorData } from "../../entities/author";
+
+export interface AuthorResponseData {
+    name: string
+    id: string
+}
+
+export interface AuthorRepositoryInterface {
+    getAllAuthors(): Promise<AuthorResponseData[]>
+    getAuthorByName(name: string): Promise<AuthorResponseData>
+    addAuthor(data: AuthorData) : Promise<AuthorResponseData>
+}

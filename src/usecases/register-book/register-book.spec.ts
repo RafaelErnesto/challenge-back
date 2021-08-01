755dc45b12ab6cd23310f22d47697e6166a28367
Rafael <rflernesto@gmail.com>
2021-07-31 23:07:58 -0400

    test: Ensure RegisterBook throws when authors provided do not exist

--- src/usecases/register-book/register-book.spec.ts
+++ src/usecases/register-book/register-book.spec.ts
@@ -1,11 +1,12 @@
 import { RegisterBook } from './register-book'
 import { BookInMemoryRepository } from '../../infra/data/in-memory/book-in-memory-repository'
 import { RegisterBookUseCase } from './register-book-use-case'
-
+import { AuthorInMemoryRepository } from '../../infra/data/in-memory/author-in-memory-repository'
 describe('Register book use case test', () => {
     const getSut = (): RegisterBookUseCase => {
         const bookRepo = new BookInMemoryRepository()
-        const sut = new RegisterBook(bookRepo)
+        const authorRepo = new AuthorInMemoryRepository()
+        const sut = new RegisterBook(bookRepo, authorRepo)
         return sut
     }
 
@@ -19,5 +20,18 @@ describe('Register book use case test', () => {
         })
        
         expect(response).toHaveProperty('id')
+    })
+
+    it('Ensure RegisterBook throws when authors provided do not exist', ()=> {
+        const sut = getSut()
+        expect(async () => {
+            await sut.registerBook({
+                name:"Test Book",
+                publication_year: 2021,
+                edition: 2,
+                authors:["test", "test2"]
+            })
+        }).rejects.toEqual('Some of the authors were not found')
+       
     })
 })

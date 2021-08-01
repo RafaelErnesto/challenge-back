f16e005f9faa32c6b0af4627b4571ebf3e6c2991
Rafael <rflernesto@gmail.com>
2021-08-01 18:21:14 -0400

    test: Ensure UpdateBookController returns statusCode 200 and updated book

---
+++ src/adapters/presentation/controllers/update-book/update-book-controller.spec.ts
@@ -0,0 +1,29 @@
+import { BaseController } from '../http/base-controller'
+import { BookInMemoryRepository } from '../../../../infra/data/in-memory/book-in-memory-repository'
+import { UpdateBook } from '../../../../usecases//update-book/update-book'
+import { AuthorInMemoryRepository } from '../../../../infra/data/in-memory/author-in-memory-repository'
+import { UpdateBookController } from './update-book-controller'
+describe('UpdateBookController test', () => {
+    const getSut = (): BaseController => {
+        const bookRepo = new BookInMemoryRepository()
+        const authorRepo = new AuthorInMemoryRepository()
+        const getBookList = new UpdateBook(bookRepo, authorRepo)
+        return new UpdateBookController(getBookList)
+    }
+
+    it('Ensure UpdateBookController returns statusCode 200 and updated book', async () => {
+        const sut = getSut()
+        const response = await sut.handle({
+            body: {
+                name: 'Updated Book',
+                edition: 1,
+                publication_year: 2001,
+                authors: ['b9jg'],
+                id: 'asm3'
+            }
+        })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.name).toEqual('Updated Book')
+    })
+})

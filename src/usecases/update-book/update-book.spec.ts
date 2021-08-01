b79301df2e1e7adaaef2133be6ec73948f29705d
Rafael <rflernesto@gmail.com>
2021-08-01 00:21:37 -0400

    test: Ensure UpdateBook usercase returns updated book on success

---
+++ src/usecases/update-book/update-book.spec.ts
@@ -0,0 +1,22 @@
+import { UpdateBookUseCase } from './update-book-use-case'
+import { BookInMemoryRepository } from '../../infra//data/in-memory/book-in-memory-repository'
+import { UpdateBook } from './update-book'
+
+describe('Update Book Test', () => {
+    const getSut = (): UpdateBookUseCase => {
+        const bookRepo = new BookInMemoryRepository()
+        const sut = new UpdateBook(bookRepo)
+        return sut
+    }
+    it('Ensure UpdateBook usercase returns updated book on success', async () => {
+        const sut = getSut()
+        const result = await sut.updateBook({
+            name: "Book 1",
+            publication_year: 2011,
+            edition:1,
+            authors:["m3sd"]
+        },"asm3")
+
+        expect(result.publication_year).toEqual(2011)
+    })
+})

517d4256c2d62f3c4cd4f21114afff25f15bf176
Rafael <rflernesto@gmail.com>
2021-07-31 23:46:24 -0400

    feat: add filter name to getBooks

--- src/infra/data/in-memory/book-in-memory-repository.ts
+++ src/infra/data/in-memory/book-in-memory-repository.ts
@@ -1,4 +1,5 @@
 import { Book, BookData } from "../../../entities/book";
+import { BookOptions } from "../../../usecases/get-book-list/get-book-list-use-case";
 import { BookRepositoryInterface } from "../../../usecases/ports/book-repository";
 import { BookResponseData } from "../../../usecases/register-book/book-response-data";
 import booksData from './books.json'
@@ -19,12 +20,18 @@ export class BookInMemoryRepository implements BookRepositoryInterface {
             id: "aqe5"
         }))
     }
-    getBooks(filters?: any): Promise<BookResponseData[]> {
-        return new Promise(resolve => resolve(this.books))
+    getBooks(filters?: BookOptions): Promise<BookResponseData[]> {
+        let result = this.books
+        if(filters?.name) {
+            result = result.filter((book: any) => book.name === filters.name)
+        }
+        return new Promise(resolve => resolve(result))
     }
+
     updateBook(data: BookData): Promise<BookResponseData> {
         throw new Error("Method not implemented.");
     }
+
     deleteBook(id: string): Promise<string> {
         throw new Error("Method not implemented.");
     }

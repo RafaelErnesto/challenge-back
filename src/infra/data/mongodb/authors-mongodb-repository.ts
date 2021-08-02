3207b3cc7905b20843ed1f2a0160b50d346fde3b
Rafael <rflernesto@gmail.com>
2021-08-01 23:53:26 -0400

    feat: MongodbRepository returns all authors from database with pagination limit of 10

---
+++ src/infra/data/mongodb/authors-mongodb-repository.ts
@@ -0,0 +1,24 @@
+import { AuthorData } from "../../../entities/author";
+import { AuthorRepositoryInterface, AuthorResponseData } from "../../../usecases/ports/author-repository";
+import { MongoHelper } from "./helper/mongo-helper";
+
+export class AuthorsMongodbRepository implements AuthorRepositoryInterface {
+    async getAllAuthors(page: number, filter?: string): Promise<AuthorResponseData[]> {
+        return await MongoHelper.getCollection('authors').find().skip(page).limit(10).toArray()
+    }
+    getAuthorByName(name: string): Promise<AuthorResponseData> {
+        throw new Error("Method not implemented.");
+    }
+    getAuthorById(id: any): Promise<AuthorResponseData> {
+        throw new Error("Method not implemented.");
+    }
+    async addAuthor(data: AuthorData): Promise<AuthorResponseData> {
+        const authorCollection = MongoHelper.getCollection('authors')
+        const author = await authorCollection.insertOne(data)
+        return new Promise(resolve => resolve({
+            name: data.name,
+            id: author.insertedId.toHexString()
+        }))
+    }
+    
+}

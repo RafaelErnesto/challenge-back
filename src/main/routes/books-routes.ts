2e441c6763228543dba8d091ceab55a7e8c9de26
Rafael <rflernesto@gmail.com>
2021-08-02 15:29:26 -0400

    feat: DELETE /api/book returns 204 on successful deletion

--- src/main/routes/books-routes.ts
+++ src/main/routes/books-routes.ts
@@ -3,8 +3,10 @@ import { adaptRoute } from '../adapters/express-router-adapter'
 import { makeRegisterBookController } from '../factories/make-register-book-controller'
 import { makeGetBookListController } from '../factories/make-get-book-list-controller'
 import { makeUpdateBookController } from '../factories/make-update-book-controller'
+import { makeDeleteBookController } from '../factories/make-delete-book-controller'
 export default (router: Router): void => {
     router.post('/book', adaptRoute(makeRegisterBookController())),
     router.get('/book', adaptRoute(makeGetBookListController())),
     router.put('/book/:id', adaptRoute(makeUpdateBookController()))
+    router.delete('/book/:id', adaptRoute(makeDeleteBookController()))
 }

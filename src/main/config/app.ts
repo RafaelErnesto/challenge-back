c8d76f2fe988fd9890eb2d3c11c2cfc9ec4a544b
Rafael <rflernesto@gmail.com>
2021-08-02 13:35:40 -0400

    test: add bodyparser to app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
-import express from 'express'
+import express  from 'express'
 import setupRoutes from './routes'
+import { json } from 'express'
 const app = express()
 
+app.use(json())
 setupRoutes(app)
 export default app

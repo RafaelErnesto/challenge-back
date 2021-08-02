1cc925521bc684481b6aa49c331be92ac64005d2
Rafael <rflernesto@gmail.com>
2021-08-02 15:11:43 -0400

    feat: adds route to update book

--- src/main/routes/books-routes.ts
+++ src/main/routes/books-routes.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-router-adapter'
 import { makeRegisterBookController } from '../factories/make-register-book-controller'
 import { makeGetBookListController } from '../factories/make-get-book-list-controller'
+import { makeUpdateBookController } from '../factories/make-update-book-controller'
 export default (router: Router): void => {
     router.post('/book', adaptRoute(makeRegisterBookController())),
-    router.get('/book', adaptRoute(makeGetBookListController()))
+    router.get('/book', adaptRoute(makeGetBookListController())),
+    router.put('/book/:id', adaptRoute(makeUpdateBookController()))
 }

26a9651dda7228bdbfb9d0e1847b5b8efc2a2171
Rafael <rflernesto@gmail.com>
2021-08-02 15:10:59 -0400

    test: Should return statusCode 200 and updated book

--- src/main/routes/books-routes.spec.ts
+++ src/main/routes/books-routes.spec.ts
@@ -155,4 +155,39 @@ describe('Books routes test', () => {
         expect(response.body.length).toBe(1)
     }, 20000)
 
+    it('Should return statusCode 200 and updated book', async () => {
+
+        const authorRepo = new AuthorsMongodbRepository()
+
+        const bookRepo = new BooksMongodbRepository()
+
+        const author = await authorRepo.addAuthor({
+            name: 'Author 1'
+        })
+
+        const book = await bookRepo.addBook({
+            name: 'Title 1',
+            edition: 2,
+            publication_year: 2014,
+            authors:[author.id]
+        })
+
+        const response = await request(app)
+        .put(`/api/book/${book.id}`)
+        .send({
+            name: 'Updated Title',
+            edition: 2,
+            publication_year: 2014,
+            authors:[author.id]
+        })
+        .expect(200)
+        expect(response.body).toMatchObject({
+            name: 'Updated Title',
+            edition: 2,
+            publication_year: 2014,
+            authors:[author.id]
+        })
+    }, 20000)
+
+
 })

1ad7303592c7347642ac40a16a7374b7210ebb79
Rafael <rflernesto@gmail.com>
2021-08-02 13:57:07 -0400

    feat: GET /api/book returns statusCode 200 and list of books

--- src/main/routes/books-routes.ts
+++ src/main/routes/books-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-router-adapter'
 import { makeRegisterBookController } from '../factories/make-register-book-controller'
-
+import { makeGetBookListController } from '../factories/make-get-book-list-controller'
 export default (router: Router): void => {
-    router.post('/book', adaptRoute(makeRegisterBookController()))
+    router.post('/book', adaptRoute(makeRegisterBookController())),
+    router.get('/book', adaptRoute(makeGetBookListController()))
 }

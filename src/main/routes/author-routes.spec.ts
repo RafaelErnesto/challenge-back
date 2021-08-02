458d553cd8117e59b3a22a6a7a1031e699fc8c47
Rafael <rflernesto@gmail.com>
2021-08-02 12:05:34 -0400

    test: GET /api/authors returns 200 and list of authors

---
+++ src/main/routes/author-routes.spec.ts
@@ -0,0 +1,41 @@
+import request from 'supertest'
+import app  from '../config/app'
+import { MongoHelper } from '../../infra/data/mongodb/helper/mongo-helper'
+import { AuthorsMongodbRepository } from '../../infra/data/mongodb/authors-mongodb-repository'
+describe('Author routes test', () => {
+    beforeAll(async () => {
+        if(process.env.MONGO_URL){
+            await MongoHelper.connect(process.env.MONGO_URL)
+        }
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach( () => {
+        MongoHelper.clearCollection('authors')
+    })
+
+    it('Should return statusCode 200 and list of authors', async () => {
+
+        const sut = new AuthorsMongodbRepository()
+
+        await sut.addAuthor({
+            name: 'Author 1'
+        })
+ 
+        await sut.addAuthor({
+            name: 'Author 2'
+        })
+ 
+        await sut.addAuthor({
+             name: 'Author 3'
+        })
+
+        const response = await request(app)
+        .get('/api/author')
+        .expect(200)
+        expect(response.body.length).toBe(3)
+    })
+})

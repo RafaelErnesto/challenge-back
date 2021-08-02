b7c6459af6d1f85d54bcaea1d3c9c2711dd131f4
Rafael <rflernesto@gmail.com>
2021-08-02 12:52:15 -0400

    chore: adapt express Request object to HttpRequest

--- src/main/adapters/express-router-adapter.ts
+++ src/main/adapters/express-router-adapter.ts
@@ -1,14 +1,14 @@
 
 import { Request, Response } from 'express'
 import { BaseController } from '../../adapters/presentation/controllers/http/base-controller'
-import { HttpRequest } from '../../adapters/presentation/controllers/http/http-request'
-
 export const adaptRoute = (controller: BaseController) => {
   return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body
-    }
-    const httpResponse = await controller.handle(httpRequest)
+    const request = {
+        query: req.query || {},
+        body:  req.body || {},
+        params: req.params || {}
+      }
+    const httpResponse = await controller.handle(request)
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }

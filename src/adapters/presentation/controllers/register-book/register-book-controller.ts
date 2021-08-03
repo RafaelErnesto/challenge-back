344c8c1d9239ed557c413a0472ca24831d6a898f
Rafael <rflernesto@gmail.com>
2021-08-03 12:20:03 -0400

    feat: RegisterBookController returns MissingParam error if parameter is missing

--- src/adapters/presentation/controllers/register-book/register-book-controller.ts
+++ src/adapters/presentation/controllers/register-book/register-book-controller.ts
@@ -3,6 +3,7 @@ import { BaseController } from "../http/base-controller";
 import { HttpRequest } from "../http/http-request";
 import { HttpResponse } from "../http/http-response";
 import { created, badRequest } from '../helpers/http-helper'
+import { MissingParameter } from "../http/errors/missing-parameter";
 
 export class RegisterBookController implements BaseController {
     private registerBook: RegisterBookUseCase
@@ -13,10 +14,29 @@ export class RegisterBookController implements BaseController {
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+            this.validate(httpRequest.body)
             const result = await this.registerBook.registerBook(httpRequest.body)
             return new Promise(resolve => resolve(created(result)))
         } catch(error) {
             return new Promise((resolve, reject) => reject(badRequest(error)))
+        }
+    }
+
+    private validate(data:any) {
+        if(!data.name) {
+            throw new MissingParameter('name')
+        }
+
+        if(!data.publication_year) {
+            throw new MissingParameter('publication_year')
+        }
+
+        if(!data.edition) {
+            throw new MissingParameter('edition')
+        }
+
+        if(!data.authors) {
+            throw new MissingParameter('authors')
         }
     }
     
